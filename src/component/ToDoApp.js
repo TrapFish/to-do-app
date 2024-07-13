c0e0bb10b6c43aeab08bea05c126b67c5e2630e4
gourav <gourav3492@gmail.com>
2024-07-13 19:39:19 +0530

    first commit

---
+++ src/component/ToDoApp.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addTodo = () => {
+    if (inputValue.trim()) {
+      setTodos([...todos, { text: inputValue, completed: false }]);
+      setInputValue('');
+    }
+  };
+
+  const toggleComplete = (index) => {
+    const newTodos = todos.map((todo, i) =>{
+       return i === index ? { ...todo, completed: !todo.completed } : todo
+    }    
+    );
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="todo-app">
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <span
+              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+              onClick={() => toggleComplete(index)}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
